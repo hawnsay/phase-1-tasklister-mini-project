cb21397aac0735bc937b97c0a007d8fd2e144b48
Shawn Chestine <shawn@Shawns-MBP.askey.com>
2023-04-16 21:07:25 -0700

    Completes Tasklister mini project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector('#create-task-form');
+  const taskList = document.querySelector('#tasks');
+  
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+  
+    const newTaskInput = document.querySelector('#new-task-description');
+    const newTask = document.createElement('li');
+    newTask.innerText = newTaskInput.value;
+  
+    // create delete button
+    const deleteButton = document.createElement('button');
+    deleteButton.innerText = 'X';
+    deleteButton.addEventListener('click', function() {
+      newTask.remove();
+    });
+    newTask.appendChild(deleteButton);
+  
+    // create edit button
+    const editButton = document.createElement('button');
+    editButton.innerText = 'Edit';
+    editButton.addEventListener('click', function() {
+      const taskText = newTask.innerText;
+      const taskTextInput = document.createElement('input');
+      taskTextInput.type = 'text';
+      taskTextInput.value = taskText;
+      newTask.innerText = '';
+      newTask.appendChild(taskTextInput);
+      taskTextInput.addEventListener('blur', function() {
+        const updatedTaskText = taskTextInput.value;
+        newTask.innerText = updatedTaskText;
+      });
+    });
+    newTask.appendChild(editButton);
+  
+    taskList.appendChild(newTask);
+  
+    newTaskInput.value = '';
+  });
+
 });
